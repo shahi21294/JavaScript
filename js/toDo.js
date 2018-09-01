f9bb66b07a542feacab8fd0bc705e0cc54d07750
AliAkbar shahi <AliAkbar.Shahi@MahanAir.Aero>
2018-09-01 15:12:25 +0430

    refactor js

--- js/toDo.js
+++ js/toDo.js
@@ -1,72 +1,88 @@
-var taskNumberArray = [];
-			for(var i = 1; i <= 100; i++){
-				taskNumberArray.push(i);
-			}
-			var taskCounter=0;
 			
-			function removeTask(id){
-					var element = document.getElementById('RemoveId-'+id);
-					element.parentNode.removeChild(element);
+			function removeTask(){
+					var taskRow=this.parentNode.parentNode;
+					var tbody=taskRow.parentNode;
+					tbody.removeChild(taskRow);
 			}
 			function addNewTask(EnterKey) {
 				if(event.key === 'Enter') {
 					if(EnterKey.value) {
 						var d1 = document.getElementById('appendtask');
-						d1.insertAdjacentHTML('afterbegin', '<tr id="RemoveId-'+taskNumberArray[taskCounter]+'" class="all active">\
-													<td style="width: 5%;">\
-																	<input type="checkbox"  id="check-'+taskNumberArray[taskCounter]+'" onclick="doneTask('+taskNumberArray[taskCounter]+');"><label for="check-'+taskNumberArray[taskCounter]+'"></label>\
-													</td>\
-													<td >\
-														<span id="textField-'+taskNumberArray[taskCounter]+'">'+EnterKey.value+'</span>\
-													</td>\
-													<td style="width: 5%;">\
-														 <a  id="removeTask" onclick="removeTask('+taskNumberArray[taskCounter]+');" ><img src="img/cross.png" ></i></a>\
-													</td>\
-												</tr>');
-						taskCounter=taskCounter+1;	
+						var newRow=document.createElement("tr");
+						var newTdOne=document.createElement("td");
+						var newTdTwo=document.createElement("td");
+						var newTdThree=document.createElement("td");
+						var checkBox=document.createElement("input");
+						var a=document.createElement("a");
+						var img=document.createElement("img");
+						var spanOne=document.createElement("span");
+						var spanTwo=document.createElement("span");
+						var label=document.createElement("label");
+						var taskName=document.createTextNode(EnterKey.value);
+						d1.appendChild(newRow);
+						newRow.appendChild(newTdOne);
+						newTdOne.appendChild(label);
+						label.appendChild(checkBox);
+						label.appendChild(spanOne);
+						newRow.appendChild(newTdTwo);
+						newTdTwo.appendChild(spanTwo);
+						spanTwo.appendChild (taskName);
+						newRow.appendChild(newTdThree);
+						newTdThree.appendChild(a);
+						a.appendChild(img);
+						checkBox.addEventListener("change", doneTask);
+						a.addEventListener("click", removeTask);
+						
+						checkBox.type="checkbox";
+						newRow.className="all active";
+						a.className="removeTask";
+						label.className="container";
+						spanOne.className="checkmark";
+						img.src="img/cross.png";
 						document.getElementById("addNewTask").value = "";
 					}else{
 						alert("Invalid Task Name");
 					}        
 				}
 			}
-			function doneTask(checkBoxId) {
-			  var checkBox = document.getElementById('check-'+checkBoxId);
-			  if (checkBox.checked == true){
-					org_html = document.getElementById("textField-"+checkBoxId).innerHTML;
-					new_html = "<strike id='strike-"+checkBoxId+"'>" + org_html + "</strike>";
-					document.getElementById("textField-"+checkBoxId).innerHTML = new_html;
-					document.getElementById("RemoveId-"+checkBoxId).setAttribute("class", "all complete");
+			function doneTask() {
+				var taskRow=this.parentNode.parentNode.parentNode;
+				var span =taskRow.getElementsByTagName("span");
+			  if (this.checked == true){
+					org_html = span[1].innerHTML;
+					new_html = "<strike>" + org_html + "</strike>";
+					span[1].innerHTML = new_html;
+					taskRow.setAttribute("class", "all complete");
 			
 			  } else {
-				document.getElementById("strike-"+checkBoxId).outerHTML = document.getElementById("strike-"+checkBoxId).innerHTML;
-				document.getElementById("RemoveId-"+checkBoxId).setAttribute("class", "all active");
+					var strike =span[1].getElementsByTagName("strike");
+					span[1].innerHTML = strike[0].innerHTML;
+					taskRow.setAttribute("class", "all active");
 			  }
 			}
 			function showHide(type) {
-			  if(type=='all'){
-				var element=document.getElementsByClassName("all")
-				for (var i=0;i<element.length;i+=1){
-				  element[i].style.display = 'table-row';
-				}
-								
-				  }else if (type=='complete'){
+				if(type=='all'){
 					var element=document.getElementsByClassName("all")
-					for (var i=0;i<element.length;i+=1){
-					  element[i].style.display = 'none';
-					}
+						for (var i=0;i<element.length;i+=1){
+						  element[i].style.display = 'table-row';
+						}
+				}else if (type=='complete'){
+					var element=document.getElementsByClassName("all")
+						for (var i=0;i<element.length;i+=1){
+						  element[i].style.display = 'none';
+						}
 					var element=document.getElementsByClassName("complete")
-					for (var i=0;i<element.length;i+=1){
-					  element[i].style.display = 'table-row';
-					}
-				  }else if (type=='active'){
+						for (var i=0;i<element.length;i+=1){
+						  element[i].style.display = 'table-row';
+						}
+				}else if (type=='active'){
 					var element=document.getElementsByClassName("all")
-					for (var i=0;i<element.length;i+=1){
-					  element[i].style.display = 'none';
-					}
+						for (var i=0;i<element.length;i+=1){
+						  element[i].style.display = 'none';
+						}
 					var element=document.getElementsByClassName("active")
-					for (var i=0;i<element.length;i+=1){
-					  element[i].style.display = 'table-row';
-					}
+						for (var i=0;i<element.length;i+=1){
+						  element[i].style.display = 'table-row';
+						}
 				  }
 			}
