da15b70c760df57b4dd3f9d9020d7b5f1e549b5a
AliAkbar shahi <AliAkbar.Shahi@MahanAir.Aero>
2018-10-08 13:51:23 +0330

    add user auth and localstorage tasks

--- js/toDoOop.js
+++ js/toDoOop.js
@@ -1,78 +1,93 @@
+var users='{\
+  "users": [\
+    {\
+      "id": "100",\
+      "userName": "ali",\
+      "password": "MTIzNDU2"\
+    },{\
+      "id": "101",\
+      "userName": "reza",\
+      "password": "MTIzNDU2"\
+    }\
+  ]\
+}';
+class User {
+  constructor(userName,password) {
+    this.userName = userName;
+    this.password = password;
+  }
+}
 var toDo = function() {
 	this.input = document.getElementById('addNewTask');
 	this.todos = JSON.parse(localStorage.getItem('taskJson') || '[]');
-	console.log(this.todos );
 	this.showTask = function() {
 			var tBody = document.getElementById("appendtask");
 				while (tBody.firstChild) {
 					tBody.removeChild(tBody.firstChild);
 				}
+			var userID=localStorage.getItem('loginID');	
 			for( var i = 0, len = this.todos.length; i < len; i++ ){
-				var d1 = document.getElementById('appendtask');
-				var newRow=document.createElement("tr");
-				var newTdOne=document.createElement("td");
-				var newTdTwo=document.createElement("td");
-				var newTdThree=document.createElement("td");
-				var checkBox=document.createElement("input");
-				var a=document.createElement("a");
-				var img=document.createElement("img");
-				var spanOne=document.createElement("span");
-				var spanTwo=document.createElement("span");
-				var label=document.createElement("label");
-				var strike=document.createElement("strike");
-				var taskName=document.createTextNode(this.todos[i].value);
-				d1.appendChild(newRow);
-				newRow.appendChild(newTdOne);
-				newTdOne.appendChild(checkBox);
-				newRow.appendChild(newTdTwo);
-				newTdTwo.appendChild(spanTwo);
-				if(this.todos[i].state==='0'){
-					spanTwo.appendChild (taskName);
-					newRow.className="all active";
-					checkBox.checked = false;
-				}else if(this.todos[i].state==='1'){
-					spanTwo.appendChild (strike);
-					strike.appendChild (taskName);
-					newRow.className="all complete";
-					checkBox.checked = true;
+				if(userID===this.todos[i].userID){
+					var d1 = document.getElementById('appendtask');
+					var newRow=document.createElement("tr");
+					var newTdOne=document.createElement("td");
+					var newTdTwo=document.createElement("td");
+					var newTdThree=document.createElement("td");
+					var checkBox=document.createElement("input");
+					var a=document.createElement("a");
+					var img=document.createElement("img");
+					var spanOne=document.createElement("span");
+					var spanTwo=document.createElement("span");
+					var label=document.createElement("label");
+					var strike=document.createElement("strike");
+					var taskName=document.createTextNode(this.todos[i].value);
+					d1.appendChild(newRow);
+					newRow.appendChild(newTdOne);
+					newTdOne.appendChild(checkBox);
+					newRow.appendChild(newTdTwo);
+					newTdTwo.appendChild(spanTwo);
+					if(this.todos[i].state==='0'){
+						spanTwo.appendChild (taskName);
+						newRow.className="all active";
+						checkBox.checked = false;
+					}else if(this.todos[i].state==='1'){
+						spanTwo.appendChild (strike);
+						strike.appendChild (taskName);
+						newRow.className="all complete";
+						checkBox.checked = true;
+					}
+					newRow.appendChild(newTdThree);
+					newTdThree.appendChild(a);
+					a.appendChild(img);
+					checkBox.addEventListener("change", this.doneTask(this.todos[i].value));
+					a.addEventListener("click",this.removeTask(this.todos[i].value));
+					checkBox.type="checkbox";
+					a.className="removeTask";
+					label.className="container";
+					img.src="img/cross.png";
+					document.getElementById("addNewTask").value = "";
 				}
-				newRow.appendChild(newTdThree);
-				newTdThree.appendChild(a);
-				a.appendChild(img);
-				checkBox.addEventListener("change", this.doneTask(this.todos[i].value));
-				a.addEventListener("click",this.removeTask(this.todos[i].value));
-				checkBox.type="checkbox";
-				a.className="removeTask";
-				label.className="container";
-				img.src="img/cross.png";
-				document.getElementById("addNewTask").value = "";
 		 }
-	}
-	this.addTask = function(entry) {
-		this.todos.push({ value: entry,state: '0'});
+	};
+	this.addTask = function(entry,userID) {
+		this.todos.push({ userID:userID, value: entry,state: '0'});
 		localStorage.setItem("taskJson", JSON.stringify(this.todos));
-	}
-	this.modifyTask = function(entry) {
-		var found = this.getTaskByName('entry');
-	}
+	};
+	
 	this.getTaskByName = function(name) {
 		for( var i = 0, len = this.todos.length; i < len; i++ ){ 
 				 if( this.todos[i].value === name )
 					 return i;
 		}
-	}
-
-	this.init = function() {
-		this.showTask();
-		this.addNewTask();
-	}  
-}
+	};
+};
 toDo.prototype.addNewTask = function() {
 		var list = this;
 		this.input.addEventListener('keydown', function(e) {
 			if(e.keyCode === 13 ) {
 				if(e.target.value.length > 0) {
-					list.addTask(e.target.value);
+					var userID=localStorage.getItem('loginID');
+					list.addTask(e.target.value,userID);
 					list.showTask();
 					e.target.value = null;
 				}else{
@@ -80,21 +95,22 @@ toDo.prototype.addNewTask = function() {
 				}        
 			}
 		  });
-}
+};
 toDo.prototype.doneTask = function(toDoName) {
 	var list = this;
+    var found='';
 	return function() {
-			if (this.checked == true){
-				var found = list.getTaskByName(toDoName);
+			if (this.checked === true){
+				found = list.getTaskByName(toDoName);
 				list.todos[found].state="1";
-		} else if(this.checked == false) {
-				var found = list.getTaskByName(toDoName);
+		} else if(this.checked === false) {
+				found = list.getTaskByName(toDoName);
 				list.todos[found].state="0";
 		}
 		localStorage.setItem("taskJson", JSON.stringify(list.todos));
 		list.showTask();
 	}
-}
+};
 toDo.prototype.removeTask = function(toDoName) {
 		var list = this;
 		return function() {
@@ -103,33 +119,77 @@ toDo.prototype.removeTask = function(toDoName) {
 			localStorage.setItem("taskJson", JSON.stringify(list.todos));
 			list.showTask();
 		}
-
-		
+};
+function init() {
+		var user=localStorage.getItem('loginID');
+		if (user != "null") {
+			document.getElementById("loginShow").style.display = "none";
+			document.getElementById("taskShow").style.display = "block";
+			document.getElementById("userWelcome").innerHTML = user;
+			new toDo().showTask();
+		} else {
+		   document.getElementById("loginShow").style.display = "block";
+		   document.getElementById("taskShow").style.display = "none";
+		}
+		new toDo().addNewTask();
+		document.getElementById("login").addEventListener("click", login);
+}  
+init();
+   
+function login() {	
+	var userName = document.getElementById('user').value;
+	var password = document.getElementById('pass').value;
+	if(user || pass){
+		if(checkUser(userName,password)!=0){
+			localStorage.setItem("loginID", userName);
+			init();
+		}else{
+			alert("Invalid UserName and Password");
+		}  
+	}else{
+		alert("Invalid UserName and Password");
+	}   
 }
-var lists = {};
-lists = new toDo().init();
+function logout() {	
+	localStorage.setItem("loginID", null);
+	init();
+}
+function checkUser(userName,password){
+	var data = JSON.parse(users);
+	var findUser = 0;
+	data['users'].forEach(function(element) {
+	if(userName===element['userName'] && btoa(password)===element['password'])
+		   findUser = userName;
+	});
+	if(findUser===0)
+		return 0;
+	else 
+		return findUser;
+}
 function showHide(type) {
-	if(type=='all'){
-		var element=document.getElementsByClassName("all")
-			for (var i=0;i<element.length;i+=1){
+	var element='';
+	var i=0;
+	if(type==='all'){
+		 element=document.getElementsByClassName("all");
+			for (i=0;i<element.length;i+=1){
 			  element[i].style.display = 'table-row';
 			}
-	}else if (type=='complete'){
-		var element=document.getElementsByClassName("all")
-			for (var i=0;i<element.length;i+=1){
+	}else if (type==='complete'){
+		element=document.getElementsByClassName("all");
+			for (i=0;i<element.length;i+=1){
 			  element[i].style.display = 'none';
 			}
-		var element=document.getElementsByClassName("complete")
-			for (var i=0;i<element.length;i+=1){
+		 element=document.getElementsByClassName("complete");
+			for (i=0;i<element.length;i+=1){
 			  element[i].style.display = 'table-row';
 			}
-	}else if (type=='active'){
-		var element=document.getElementsByClassName("all")
-			for (var i=0;i<element.length;i+=1){
+	}else if (type==='active'){
+		 element=document.getElementsByClassName("all");
+			for (i=0;i<element.length;i+=1){
 			  element[i].style.display = 'none';
 			}
-		var element=document.getElementsByClassName("active")
-			for (var i=0;i<element.length;i+=1){
+		 element=document.getElementsByClassName("active");
+			for (i=0;i<element.length;i+=1){
 			  element[i].style.display = 'table-row';
 			}
 	  }
