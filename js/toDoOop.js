4fd2975eb75a4543f6af18cda55d4b1e377d109f
AliAkbar shahi <AliAkbar.Shahi@MahanAir.Aero>
2018-10-22 19:06:46 +0330

    separate some function

--- js/toDoOop.js
+++ js/toDoOop.js
@@ -4,34 +4,35 @@
 	var users=[{ ID:101, userName: "ali",password: "123456"},{ ID:102, userName: "reza",password: "123456"}];
 	
 	
-	function createTodo(taskName,userID){
-		todos.push({userID:userID,value:taskName,state: '0'});
+	function createTask(taskName){
+		todos.push({userID:getCurrenttUser(),value:taskName,is_completed: '0'});
 		localStorage.setItem("taskJson", JSON.stringify(todos));
+		
 		document.querySelector("#addNewTask").value = "";
 	}
 	function getCurrenttUser(){
 		return localStorage.getItem('loginID');
 	}
-	function doneTask(toDoName) {
+	function changeTaskStatus(toDoName) {
 		var found='';
 		return function() {
 				if (this.checked === true){
 					found = getTaskByName(toDoName);
-					todos[found].state="1";
+					todos[found].is_completed="1";
 			} else if(this.checked === false) {
 					found = getTaskByName(toDoName);
-					todos[found].state="0";
+					todos[found].is_completed="0";
 			}
 			localStorage.setItem("taskJson", JSON.stringify(todos));
 			init();
 		}
-	};
+	}
 	function getTaskByName(name) {
-		for( var i = 0, len = todos.length; i < len; i++ ){ 
+	for( var i = 0, len = todos.length; i < len; i++ ){
 				 if(todos[i].value === name )
 					 return i;
 		}
-	};
+	}
 	function removeTask(toDoName) {
 		return function() {
 			var found = getTaskByName(toDoName);
@@ -39,7 +40,7 @@
 			localStorage.setItem("taskJson", JSON.stringify(todos));
 			init();
 		}
-	};
+	}
 	function getUserID(userName,password){
 		var findUser = 0;
 		users.forEach(function(element) {
@@ -51,11 +52,11 @@
 		else 
 			return findUser;
 	}
-	function getUserByID(userID){
+	function getUserByID(){
 		var findUser = 0;
 		users.forEach(function(element) {
-			if(element['ID']==userID)
-				   findUser = element['userName'];
+			if(element['ID']==getCurrenttUser())
+				    findUser = element['userName'];
 			}
 		);
 		if(findUser===0)
@@ -66,22 +67,26 @@
 	taskInput.addEventListener('keydown', function(e) {
 		if (e.which == 13) {
 			if(e.target.value.length > 0) {
-				createTodo(e.target.value,getCurrenttUser());
+				createTask(e.target.value);
 				init();
 			}else{
 				alert("Invalid Task Name");
 			}        
 		}
 	});
+	function destroyTask(){
+		var tBody = document.getElementById("appendTask");
+		while (tBody.firstChild) {
+			tBody.removeChild(tBody.firstChild);
+		}
+	}
 	function showTask() {
-			var tBody = document.getElementById("appendtask");
-				while (tBody.firstChild) {
-					tBody.removeChild(tBody.firstChild);
-				}
-			var userID=localStorage.getItem('loginID');	
-			for( var i = 0, len = todos.length; i < len; i++ ){
-				if(userID==todos[i].userID){
-					var d1 = document.getElementById('appendtask');
+			destroyTask();
+			var userTask =  todos.filter(function(taskObject) {
+				return taskObject.userID == getCurrenttUser();
+			});
+			for( var i = 0, len = userTask.length; i < len; i++ ){
+					var d1 = document.getElementById('appendTask');
 					var newRow=document.createElement("tr");
 					var newTdOne=document.createElement("td");
 					var newTdTwo=document.createElement("td");
@@ -93,18 +98,17 @@
 					var spanTwo=document.createElement("span");
 					var label=document.createElement("label");
 					var strike=document.createElement("strike");
-					var taskName=document.createTextNode(todos[i].value);
-					
+					var taskName=document.createTextNode(userTask[i].value);
 					d1.appendChild(newRow);
 					newRow.appendChild(newTdOne);
 					newTdOne.appendChild(checkBox);
 					newRow.appendChild(newTdTwo);
 					newTdTwo.appendChild(spanTwo);
-					if(todos[i].state==='0'){
+					if(userTask[i].is_completed==='0'){
 						spanTwo.appendChild (taskName);
 						newRow.className="all active";
 						checkBox.checked = false;
-					}else if(todos[i].state==='1'){
+					}else if(userTask[i].is_completed==='1'){
 						spanTwo.appendChild (strike);
 						strike.appendChild (taskName);
 						newRow.className="all complete";
@@ -113,21 +117,19 @@
 					newRow.appendChild(newTdThree);
 					newTdThree.appendChild(a);
 					a.appendChild(img);
-					checkBox.addEventListener("change", doneTask(todos[i].value));
-					a.addEventListener("click",removeTask(todos[i].value));
+					checkBox.addEventListener("change", changeTaskStatus(userTask[i].value));
+					a.addEventListener("click",removeTask(userTask[i].value));
 					checkBox.type="checkbox";
 					a.className="removeTask";
 					label.className="container";
 					img.src="img/cross.png";
-				}
 		 }
-	};	
+	}
 	function init() {
-		var userID=localStorage.getItem('loginID');
-		if (getUserByID(userID)) {
+		if (getUserByID()) {
 			document.getElementById("loginShow").style.display = "none";
 			document.getElementById("taskShow").style.display = "block";
-			document.getElementById("userWelcome").innerHTML = getUserByID(userID);
+			document.getElementById("userWelcome").innerHTML = getUserByID();
 			document.getElementById("logout").addEventListener("click", logout);	
 			showTask();
 		} else {
@@ -147,39 +149,34 @@
 				alert("Invalid UserName and Password");
 		}else{
 				alert("Invalid UserName and Password");
-		}   
+		}  
+			
 	}
 	function logout() {	
 		localStorage.setItem("loginID", "null");
 		init();
 	}
 	function filterTask(type) {
-		var element='';
-		var i=0;
-		if(type==='all'){
-			 element=document.getElementsByClassName("all");
-				for (i=0;i<element.length;i+=1){
-				  element[i].style.display = 'table-row';
-				}
-		}else if (type==='complete'){
-			element=document.getElementsByClassName("all");
-				for (i=0;i<element.length;i+=1){
-				  element[i].style.display = 'none';
-				}
-			 element=document.getElementsByClassName("complete");
-				for (i=0;i<element.length;i+=1){
-				  element[i].style.display = 'table-row';
-				}
-		}else if (type==='active'){
-			 element=document.getElementsByClassName("all");
-				for (i=0;i<element.length;i+=1){
-				  element[i].style.display = 'none';
-				}
-			 element=document.getElementsByClassName("active");
-				for (i=0;i<element.length;i+=1){
-				  element[i].style.display = 'table-row';
-				}
-		  }
+		
+		switch (type){
+			case "all": 
+				showHideTaskByType("all","table-row");
+			break;
+			case "complete" : 
+				showHideTaskByType("complete","table-row");
+				showHideTaskByType("all","none");
+			break;
+			case "active" :
+				showHideTaskByType("active","table-row");
+				showHideTaskByType("all","none");
+			break;
+		}
+	}
+	function showHideTaskByType(type) {
+		var element=document.getElementsByClassName(type);
+		for (var i=0;i<element.length;i+=1){
+			element[i].style.display = 'table-row';
+		}
 	}
 	init();
 
